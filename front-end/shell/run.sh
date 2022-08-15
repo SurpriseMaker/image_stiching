be315519cfaaa9f0d387c8559f102b871e618d14
Bo Bo3 Cao <caobo3@Lenovo.com>
2022-08-15 17:27:26 +0800

    1. Add image information printing into log CSV 2. Adjust net rate array.

--- front-end/shell/run.sh
+++ front-end/shell/run.sh
@@ -6,7 +6,7 @@ img_ftp_download_path=ftp://${server_ip_address}/out/output.png
 ftp_log_path=ftp://${server_ip_address}/log/
 cgi_stich_path=${server_ip_address}/cgi-bin/stich_image.py
 cgi_clean_path=${server_ip_address}/cgi-bin/clean.py
-net_rate_array=(100m 50m)
+net_rate_array=(16m 8m 4m 2m 1m 512k 256k 128k 64k)
 log_file=log$(date +%Y%m%d)_$(date +%H%M%S).csv
 
 function clean(){
@@ -67,7 +67,8 @@ echo   "$samplename,\
 	$time_stich_img_remotely_duration_ms,\
 	$time_download_img_duration_ms,\
 	$time_local_stich_duration_ms,\
-	$time_remote_faster_ms\
+	$time_remote_faster_ms,\
+	$img_info\
 	" >>$log_file
 
 curl -T $log_file $ftp_log_path
@@ -78,7 +79,7 @@ function stich_one_sample_locally(){
 #run image stiching locally
 
 time_local_stich_start=$(date +%s%N)
-#python image_stitching.py --images $img_src_path --output output.png --crop 1
+python image_stitching.py --images $img_src_path --output output.png --crop 1
 time_local_stich_end=$(date +%s%N)
 time_local_stich_duration_ms=$[(time_local_stich_end-time_local_stich_start)/1000000]
 echo "Image stiched locally duration =$time_local_stich_duration_ms  ms"
@@ -87,6 +88,15 @@ echo "Image stiching locally finished."
 function stich_one_sample(){
     img_path=$1
     
+    for file in ${img_path}/*
+    do
+    if test -f $file
+    then
+	    img_info=$(identify $file)
+	    break
+    fi
+    done
+
     echo "Step 1. Starting image stiching locally.This may take a long time, please wait..."
     stich_one_sample_locally $img_path
 
@@ -100,13 +110,14 @@ function stich_one_sample(){
 
 function stich_all_samples(){
 	echo   "Image Stiching Data Result" > $log_file
-	echo   "Sample name,\
+	echo   "Sample Name,\
 		Net Rate,\
 		Upload Image Duration,\
 		Imaage Stiched Remotely Duration,\
 		Image Download Duraiton,\
 		Image Stiched Locally Duration,\
-		Remote Faster(ms)\
+		Remote Faster(ms),\
+		Image Informations\
 		">>$log_file
 	for folder in ${img_root_path}/*
 	do
